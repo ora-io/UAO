6dc0a64bf3689029b266ce93e5b882ff55796085
Norman <nom4dv3@gmail.com>
2024-08-06 18:45:49 +0800

    feat: add error VerifyFailed() in AsyncOracleValidity

--- contracts/AsyncOracleValidity.sol
+++ contracts/AsyncOracleValidity.sol
@@ -5,6 +5,7 @@ import "./AsyncOracle.sol";
 import {IValidityAsync} from "./interface/IAsyncOracle.sol";
 
 error AlreadyInvoked();
+error VerifyFailed();
 
 abstract contract AsyncOracleValidity is AsyncOracle, IValidityAsync {
     constructor(bytes4 _callbackFunctionSelector) AsyncOracle(_callbackFunctionSelector) {}
@@ -19,8 +20,9 @@ abstract contract AsyncOracleValidity is AsyncOracle, IValidityAsync {
         // only invoke once in validity style async oracle
         if (invoked[requestId]) revert AlreadyInvoked();
         invoked[requestId] = true;
-
-        _verify(requestId, output, proof);
+        // verify output
+        if (!_verify(requestId, output, proof)) revert VerifyFailed();
+        // invoke
         _invoke(requestId, output);
         // _updateGasPrice();
     }
